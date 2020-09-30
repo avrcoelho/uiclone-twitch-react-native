12d64a304825c9307521f36e3080d580b24cb5fd
Andre Coelho <andre.coelho@axur.com>
2020-09-30 08:58:47 -0300

    feat: init Followibng page

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container, HeadingText } from "./styles";
+
+const Heading: React.FC = ({ children }) => {
+  return (
+    <Container>
+      <HeadingText></HeadingText>
+    </Container>
+  );
+};
+
+export default Heading;
